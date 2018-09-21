19406e049354ecc7399c582276a60a0998c7af53
ankit_kr <ankit@transneuron.com>
2018-09-21 17:35:58 +0530

    first commit

---
+++ Gin-Web/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	engine.GET("/sandeep", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello")
+	})
+	
+
+
+
+	// run server on PORT
+	engine.Run(port())
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
